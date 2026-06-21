9b66f47637c7cd7748ebfb324e84a307eaaf6b7e
isucon <isucon@example.com>
2026-06-21 16:18:50 +0900

    k

--- ABC/460/c.cpp
+++ ABC/460/c.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 #define ll long long
+#define ld long double
 #define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
 #define FOR(i, a, b) for (ll i = (a); i < (ll)(b); i++)
 #define FORR(i, a, b) for (ll i = (a); i <= (ll)(b); i++)
@@ -13,8 +14,6 @@ using namespace std;
 #define pb push_back
 #define pu push
 #define COUT(x) cout << (x) << "\n"
-#define PQ(x) priority_queue<x>
-#define PQR(x) priority_queue<x, vector<x>, greater<x>>
 #define YES(n) cout << ((n) ? "YES\n" : "NO\n")
 #define Yes(n) cout << ((n) ? "Yes\n" : "No\n")
 #define mp make_pair
@@ -22,15 +21,29 @@ using namespace std;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 typedef tuple<ll, ll, ll> tll;
-const ll MOD = 1000000007LL;
-const ll INF = 1LL << 60;
+using u64 = unsigned long long;
+using vii = vector<int>;
+using vvii = vector<vii>;
 using vll = vector<ll>;
 using vb = vector<bool>;
 using vvb = vector<vb>;
 using vvll = vector<vll>;
+using vvvll = vector<vvll>;
 using vstr = vector<string>;
 using vc = vector<char>;
 using vvc = vector<vc>;
+
+template <class T>
+using PQ = priority_queue<T>;
+
+template <class T>
+using PQR = priority_queue<T, vector<T>, greater<T>>;
+
+// const ll MOD = 1e9+7LL;
+const ll MOD = 998244353LL;
+const ll INF = 1LL << 62;
+const double INF_D = numeric_limits<double>::infinity();
+
 template <class T>
 constexpr void printArray(const vector<T> &vec, char split = ' ')
 {
@@ -62,66 +75,44 @@ inline bool chmin(T &a, T b)
 }
 ll dx[4] = {0, 1, 0, -1};
 ll dy[4] = {1, 0, -1, 0};
-// gcd lcm
-template <class T>
-T gcd(T a, T b)
+bool isIn(ll nx, ll ny, ll h, ll w)
 {
-  return b ? gcd(b, a % b) : a;
-}
-
-template <class T>
-T lcm(T a, T b)
-{
-  return a / gcd(a, b) * b;
-}
-template <class T>
-void superset_mobius(vector<T> &f)
-{
-  int n = f.size();
-  assert((n & (n - 1)) == 0);
-  for (int b = 1; b < n; b <<= 1)
+  if (nx >= 0 && nx < h && ny >= 0 && ny < w)
   {
-    for (int l = 0; l < n; l += b << 1)
-    {
-      for (int i = 0; i < b; i++)
-      {
-        f[l + i] -= f[l + b + i];
-      }
-    }
+    return true;
   }
+  return false;
 }
 int main()
 {
-  ll n, m, y;
-  cin >> n >> m >> y;
+  ll n, m;
+  cin >> n >> m;
   vll a(n);
-  rep(i, n) cin >> a[i];
-  vll c(1 << n, 0);
-  rep(bit, 1 << n)
+  multiset<ll> s;
+  rep(i, n)
   {
-    __int128_t l = 1;
-
-    rep(i, n)
-    {
-      if (bit >> i & 1)
-      {
-        l = lcm(l, a[i]);
-      }
-      if (l > y)
-      {
-        break;
-      }
-    }
-    c[bit] = y / l;
+    cin >> a[i];
   }
-  superset_mobius(c);
-  ll ans = 0;
-  rep(bit, 1 << n)
+  sort(all(a));
+  vll b(m);
+  rep(i, m)
   {
-    if (__builtin_popcount(bit) == m)
+    cin >> b[i];
+    s.insert(b[i]);
+  }
+  sort(all(b));
+  ll ans = 0;
+  repR(i, n)
+  {
+    ll x = a[i] * 2;
+    auto it = s.upper_bound(x);
+    if (it == s.begin())
     {
-      ans += c[bit];
+      break;
     }
+    it--;
+    s.erase(it);
+    ans++;
   }
   cout << ans << endl;
 }
@@ -130,5 +121,13 @@ ios::sync_with_studio(false);
 next_permutation(v.begin(), v.end())
 
 cout << fixed << setprecision(10);
+__int128
 
-__builtin_popcount(i)*/
+//ソート済み
+v.erase(unique(v.begin(), v.end()), v.end());
+__builtin_popcountll(i)
+
+// maskからnowのビットだけ削除
+mask & ~(1 << now)
+
+*/
